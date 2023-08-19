ab74e51e715f5cdd06476802fe604be630a1d76e
Abhishek Jadhav <abhijad200@gmail.com>
2023-08-20 00:42:22 +0530

    Updated template

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ env.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json({limit: '30mb'}));
+app.use(express.urlencoded({limit: '30mb'}));
 app.use(express.json());
 app.use(cors());
 app.use(pdfRoute);
