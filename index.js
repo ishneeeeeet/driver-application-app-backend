aa01489b287791cf1fe01034b02977f5f8d1ec52
Abhishek Jadhav <abhijad200@gmail.com>
2023-08-20 02:09:23 +0530

    Updated cors

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ const port = process.env.PORT || 8000;
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit: '30mb'}));
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'https://driver-application-app.vercel.app/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 app.use(pdfRoute);
 app.listen(port, () => console.log(`server is running on port ${port}`));
