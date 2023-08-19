aafeb23b59ff7040cd7b400d2ecaf02622634499
Abhishek Jadhav <abhijad200@gmail.com>
2023-08-20 01:28:39 +0530

    Reverted file name

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 8000;
 app.use(express.json({limit: '30mb'}));
 app.use(express.urlencoded({limit: '30mb'}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(pdfRoute);
 app.listen(port, () => console.log(`server is running on port ${port}`));
