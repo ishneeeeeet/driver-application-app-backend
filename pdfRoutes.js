415c5d4ee6d3c2bfc3a87495aee77dbec7d29c8b
Abhishek Jadhav <abhijad200@gmail.com>
2023-09-17 21:03:17 +0530

    Removed unused routes

--- pdfRoutes.js
+++ pdfRoutes.js
@@ -3,7 +3,7 @@ const pdfRoute = express.Router();
 const { createPdf, fetchPdf, sendPdf } = require("./pdfController");
 
 pdfRoute.post("/createPdf", createPdf); // to generate pdf
-pdfRoute.get("/fetchPdf", fetchPdf); // to generate pdf
-pdfRoute.post("/sendPdf", sendPdf); // send pdf to mail
+// pdfRoute.get("/fetchPdf", fetchPdf); // to generate pdf
+// pdfRoute.post("/sendPdf", sendPdf); // send pdf to mail
 
 module.exports = pdfRoute;
